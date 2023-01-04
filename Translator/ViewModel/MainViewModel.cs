0e85ebef566801cb11b27aa4542563e43edf9026
gjaeu0112 <86722530+gjaeu0112@users.noreply.github.com>
2023-01-04 17:42:18 +0900

    Translator: UI Modify
    MainWindow > WithLogView > SingleTranslator 순으로 들어가도록

---
+++ Translator/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Translator.Core;
+using Translator.Views;
+
+namespace Translator.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand WithLogViewCommand;
+        public RelayCommand CompareViewCommand;
+        public RelayCommand MinimalViewCommand;
+        public WithLogView WithLogV { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            WithLogV = new WithLogView();
+            CurrentView = WithLogV;
+
+            WithLogViewCommand = new RelayCommand(o => { CurrentView = WithLogV; });
+        }
+    }
+}
